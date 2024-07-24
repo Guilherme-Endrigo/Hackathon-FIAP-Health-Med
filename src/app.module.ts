71e85d6773a4694ed4b78bc0444e0c445aaf8345
Jobson Ribeiro <jobson.analistati@gmail.com>
2024-07-24 18:20:38 -0300

    Ajusts secrets

--- src/app.module.ts
+++ src/app.module.ts
@@ -32,11 +32,11 @@ import { DoctorAdapter } from './infrastructure/adapters/doctor.adapter';
       config: {
         client: 'mysql2',
         connection: {
-          host: 'localhost',
-          port: 3307,
-          user: 'clinicuser',
-          password: 'clinicpassword',
-          database: 'clinic_fiap_project',
+          host: process.env.DB_HOST,
+          port: Number(process.env.DB_PORT),
+          user: process.env.DB_USER,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_DATABASE,
         },
       },
     }),
--- src/config/jwt.config.ts
+++ src/config/jwt.config.ts
@@ -1,4 +1,4 @@
 // src/config/jwt.config.ts
 export const jwtConstants = {
-    secret: 'JC)SJD*J(*12ns89asdn1',
+    secret: process.env.JWT_SECRET,
 };
